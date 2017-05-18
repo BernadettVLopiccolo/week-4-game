43ce1318d19b88521e135612d4c5d349aab85404
Bernadett Villam Lopiccolo <bernadettvillam@yahoo.com>
2017-05-18 11:58:23 -0700

    changes in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,9 +8,9 @@ var losses = 0;
 
 //value numbers for each crystal
 var numberOptions = [10, 7, 2, 21];
+ 
 
 function reset() {
-
   counter = 0;
   targetNumber = Math.floor((Math.random() * 100) + 20);
   $("#randomNumber").text(targetNumber);
@@ -20,6 +20,7 @@ function reset() {
 function writeWinLose() {
   $("#win-counter").text(wins);
   $("#loss-counter").text(losses);
+
 }
 
 
@@ -28,33 +29,37 @@ function updateGame(id) {
   counter += increment;
   $("#currentScoreCounter").text(counter);
   console.log("New score: " + counter);
-
+  $("#win_or_lose").text(" ");
 
   if (counter === targetNumber) {
     wins++;
     $("#win_or_lose").text("You win!");
-     
+    $("#currentScoreCounter").text(" "); 
     console.log("You win!");
-     reset();
-     writeWinLose();
+ 
+    
+      reset();
+    
+      writeWinLose();
     
    
 
   } else if (counter >= targetNumber) {
     losses++;
     $("#win_or_lose").text("You Lose!");
-    
+    $("#currentScoreCounter").text(" "); 
      console.log("You lose!");
-    reset();
-    writeWinLose();
+      reset();
+      writeWinLose();
    
   }
 }
 
-//setting up value for each crystal
+
 $(".crystal-image").on("click", function() {
   var id = $(this).attr("data-crystal-id");
   updateGame(parseInt(id));
+
 });
 
 reset();
